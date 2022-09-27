82c3f94d16a961cef2400cac51f5a719d91ebdfe
harii-in <charin1130@naver.com>
2022-09-28 01:39:21 +0900

    #18258

---
+++ 18258.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+queue = deque([])
+
+def push(X):
+    queue.append(X)
+def pop():
+    if len(queue) == 0:
+        print(-1)
+    else:
+        print(queue.popleft())
+def size():
+    print(len(queue))
+def empty():
+    if len(queue) == 0:
+            print(1)
+    else:
+        print(0)
+def front():
+    if len(queue) == 0:
+        print(-1)
+    else:
+        print(queue[0])
+def back():
+    if len(queue) == 0:
+        print(-1)
+    else:
+        print(queue[-1])
+
+
+for _ in range(N):
+    tmp = sys.stdin.readline().split()
+    if tmp[0] == 'push':
+        push(tmp[1])
+    elif tmp[0] == 'pop':
+        pop()
+    elif tmp[0] == 'size':
+        size()
+    elif tmp[0] == 'empty':
+        empty()
+    elif tmp[0] == 'front':
+        front()
+    elif tmp[0] == 'back':
+        back()
