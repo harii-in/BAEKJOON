c826dc93c3fd137d2527f9c9960bbea610ffd9bb
harii-in <charin1130@naver.com>
2022-10-05 15:08:28 +0900

    #15657

---
+++ 15657.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+s = []
+
+def DFS(start):
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(start, N):
+        s.append(num[i])
+        DFS(i)
+        s.pop()
+
+num.sort()
+DFS(0)
