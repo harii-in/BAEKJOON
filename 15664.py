d236f6c96ae3a5784ac08199d65f28ec55b8ed8b
harii-in <charin1130@naver.com>
2022-10-06 17:12:45 +0900

    #15664

---
+++ 15664.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+
+num.sort()
+visited = [False] * N
+s = []
+
+def DFS(start):
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    
+    overlap = 0
+    for i in range(start, N):
+        if not visited[i] and overlap != num[i]:
+            visited[i] = True
+            s.append(num[i])
+            overlap = num[i]
+            DFS(i+1)
+            visited[i] = False
+            s.pop()
+
+DFS(0)
